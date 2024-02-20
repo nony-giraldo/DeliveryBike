3207a85b7c53573db19dd73957fe959a6f1d7bc0
Nony Giraldo Castillo <nony.giraldo@gestionysistemas.com>
2024-02-20 15:36:09 -0500

    Agregar archivos de proyecto.

---
+++ ConsoleApp1/DeliveryBikes.cs
@@ -0,0 +1,41 @@
+﻿namespace ConsoleApp1;
+public static class DeliveryBikes
+{
+    public static int NumberOfBicycles(int[] packageWeights, int N)
+    {
+        var inOnPackage = packageWeights.Count(x => x == N);
+        var packageWeightsNonN = packageWeights.Where(x => x != N).ToArray();
+
+        var pairPackages = CountPairs(packageWeightsNonN, N);
+
+        return inOnPackage + pairPackages;
+    }
+    public static int CountPairs(int[] packageWeights, int N)
+    {
+        Array.Sort(packageWeights);
+        int left = 0;
+        int right = packageWeights.Length - 1;
+        int pairs = 0;
+
+        while (left < right)
+        {
+            int sum = packageWeights[left] + packageWeights[right];
+            if (sum == N)
+            {
+                pairs++;
+                left++;
+                right--;
+            }
+            else if (sum < N)
+            {
+                left++;
+            }
+            else
+            {
+                right--;
+            }
+        }
+
+        return pairs;
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,8 @@
+﻿using ConsoleApp1;
+
+int[] packageWeights = [5, 3, 2, 5, 6, 1, 4, 7, 9];
+int N = 10;
+
+int bicycles = DeliveryBikes.NumberOfBicycles(packageWeights, N);
+Console.WriteLine($"Number of bicycles : {bicycles}");
+
